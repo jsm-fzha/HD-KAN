46e46d0869a3398dc62f0a6b193e2edb76cbdb30
jsm-fzha <jsm12521@gmail.com>
2026-01-22 11:21:08 +0800

    Delete HD-KAN directory

--- HD-KAN/scripts/all_run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call scripts/LongForecasting/HDKAN.bat
-call scripts/ShortForecasting/HDKAN_s1.bat
-call scripts/ShortForecasting/HDKAN_s2.bat
-
-echo All bat files finished.
-pause
--- HD-KAN/scripts/all_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bash scripts/LongForecasting/HDKAN.sh
-bash scripts/ShortForecasting/HDKAN_s1.sh
-bash scripts/ShortForecasting/HDKAN_s2.sh
-
-echo "All sh files finished."
-read -p "Press Enter to exit..."
